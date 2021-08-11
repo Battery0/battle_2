51ad03440b4215dff9fe5743c3fb92f5c91393f3
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-08-11 16:57:55 +0100

    Refactored feature tests to use - #sign_in_and_play

--- spec/features/form_spec.rb
+++ spec/features/form_spec.rb
@@ -1,9 +1,6 @@
 feature 'Testing that players can enter their names and see them' do
   scenario 'Player enters name into form' do
-    visit('/')
-    fill_in('name1', with: 'Ian')
-    fill_in('name2', with: 'Hotu')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('Welcome, Ian and Hotu!')
   end
 end
